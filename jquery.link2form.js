8899cbf9971c8bba1581dffa89bcd4d52db89944
YE Qing <2chin.yip@gmail.com>
2015-02-24 11:34:00 +0800

    bind to click event and decode the param value

--- jquery.link2form.js
+++ jquery.link2form.js
@@ -1,12 +1,12 @@
 (function($){
         $.fn.link2form = function() {
-                this.filter('a').each(function() {
+                this.filter('a').click(function() {
                         var request = $(this).attr('href').split('?');
                         var action = request[0], params = request[1].split('&');
                         var form = $('<form/>', {action: action, method: 'post'}).appendTo('body');
                         for (var i in params) {
                                 var p = params[i].split('=');
-                                var key = p[0], value = p[1];
+                                var key = p[0], value = decodeURIComponent(p[1]);
                                 $('<input/>', {type: 'hidden', name: key, value: value}).appendTo(form)
                         }
                         form.submit();
